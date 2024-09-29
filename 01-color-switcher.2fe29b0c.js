88b6f4bc1c217b13a8121dbb0a32b0343711320f
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-09-29 16:19:39 +0000

    Deploying to gh-pages from @ mmax1701/goit-js-hw-09-new@696ee6ee07d55e8743c9a5509d0bfa27f05dd535 🚀

---
+++ 01-color-switcher.2fe29b0c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(n){a=setInterval((function(){n.target.parentNode.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}));var a=null}();
+//# sourceMappingURL=01-color-switcher.2fe29b0c.js.map
