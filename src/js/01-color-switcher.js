696ee6ee07d55e8743c9a5509d0bfa27f05dd535
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-09-29 19:19:20 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const start = document.querySelector('button[data-start]')
+const stop = document.querySelector('button[data-stop]')
+
+start.addEventListener('click', onStart)
+stop.addEventListener('click', onStop)
+
+let changeColor = null;
+
+function onStart(evt) {
+    changeColor = setInterval(() => {
+        evt.target.parentNode.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    start.disabled = true;
+    stop.disabled = false;
+
+}
+
+function onStop() {
+    clearInterval(changeColor);
+    start.disabled = false;
+    stop.disabled = true;
+}
+
+ function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
 
