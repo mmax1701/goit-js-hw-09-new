daad3c017ca57b5322e0f198d59b11e65d432720
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-09-30 20:38:34 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,17 +5,24 @@ import "flatpickr/dist/flatpickr.min.css";
 const input = document.querySelector('#datetime-picker')
 const start = document.querySelector('button[data-start]')
 
+const date = new Date();
+start.disabled = true;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    onClose(selectedDates) {
+        console.log(selectedDates[0]);
+        if (date.getTime() > selectedDates[0].getTime()) {
+            window.alert("Please choose a date in the future");
+        }
   },
 };
 
 const fp = flatpickr(input, options);
+
 
 
 
