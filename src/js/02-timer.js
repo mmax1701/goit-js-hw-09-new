34e546f7e5261efef0817174c7561812f352be72
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-10-01 21:27:27 +0300

    task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,8 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 
 const input = document.querySelector('#datetime-picker')
 const start = document.querySelector('button[data-start]')
@@ -21,26 +23,23 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
       const date = new Date();
-      // console.log(selectedDates[0]);
         if (date.getTime() > selectedDates[0].getTime()) {
-            window.alert("Please choose a date in the future");
+            Notify.failure("Please choose a date in the future");
         } else {
             start.disabled = false;
-          // getDate = convertMs((selectedDates[0].getTime() - date.getTime()))
-          // getDate = selectedDates[0].getTime() - date.getTime()
           getDate = selectedDates[0].getTime()
         }
   },
 };
 
-const fp = flatpickr(input, options);
+flatpickr(input, options);
 
 
 start.addEventListener('click', onClick)
 
 function onClick() {
     start.disabled = true;
-  // const newDate = convertMs(getDate)
+
   
  const timer = setInterval(() => {
     const date = getDate - (new Date().getTime());
@@ -49,7 +48,13 @@ function onClick() {
     hours.textContent = addLeadingZero(convertMs(date).hours);
     minutes.textContent = addLeadingZero(convertMs(date).minutes);
     seconds.textContent = addLeadingZero(convertMs(date).seconds);
+   
+   if (days.textContent === '00' && hours.textContent === '00' && minutes.textContent === '00' && seconds.textContent === '00') {
+     clearInterval(timer);
+   }
  }, 1000)
+  
+  
 }
 
 function addLeadingZero(value) {
