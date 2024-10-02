eace64eadffa98483e12aec2531a93bd1a90d14c
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-10-02 16:33:58 +0000

    Deploying to gh-pages from @ mmax1701/goit-js-hw-09-new@96ec18f9c4b3a2c60403e6bfa06390b90900884c 🚀

---
+++ 03-promises.1c949354.js
@@ -0,0 +1,2 @@
+document.querySelector('input[name="delay"]'),document.querySelector('input[name="step"]'),document.querySelector('input[name="amount"]'),document.querySelector("button");
+//# sourceMappingURL=03-promises.1c949354.js.map
