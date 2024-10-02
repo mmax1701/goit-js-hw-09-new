96ec18f9c4b3a2c60403e6bfa06390b90900884c
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-10-02 19:33:30 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,8 @@
+const delay = document.querySelector('input[name="delay"]')
+const step = document.querySelector('input[name="step"]')
+const amount = document.querySelector('input[name="amount"]')
+const submit = document.querySelector('button')
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +11,4 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
