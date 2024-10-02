db4eb25f1ff53f4679a82fec33bf45bc31a7d00f
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-10-02 22:11:19 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,14 +1,40 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const delay = document.querySelector('input[name="delay"]')
 const step = document.querySelector('input[name="step"]')
 const amount = document.querySelector('input[name="amount"]')
-const submit = document.querySelector('button')
+const form = document.querySelector('.form')
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+form.addEventListener('submit', onSubmit)
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  let delayVal = Number(delay.value)
+  const stepVal = Number(step.value)
+  const amountVal = Number(amount.value)
+  
+  for (let i = 1; i <= amountVal; i += 1) {
+    createPromise(i, delayVal)
+      .then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+      .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      })
+    delayVal += stepVal
   }
 }
 
+function createPromise(position, delay) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+       const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    res({position, delay})
+  } else {
+    rej({position, delay})
+  }
+    }, delay)
+  })
+}
